f5492804474f5c8459a8016d950ead76b076a4d3
Howard Mao <zhehao.mao@gmail.com>
2013-12-30 16:50:44 -0500

    add md5unit to aggregate two chunk_crunchers together

--- rtl/md5cracker.v
+++ rtl/md5cracker.v
@@ -3,8 +3,14 @@ module md5cracker (
     output [3:0] leds
 );
 
-chunk_cruncher cc (
-    .clk (clock_50)
+md5unit md5 (
+    .clk (clock_50),
+    .reset (2'b00),
+    .start (2'b00),
+
+    .write (1'b0),
+    .writedata (31'd0),
+    .writeaddr (5'd0)
 );
 
 endmodule

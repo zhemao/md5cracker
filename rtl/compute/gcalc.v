f50a061b64d2c2b874f9e9b031be56aa263a3d68
Howard Mao <zhehao.mao@gmail.com>
2013-12-30 15:03:20 -0500

    replace multiplications with shifts and add/sub

--- rtl/compute/gcalc.v
+++ rtl/compute/gcalc.v
@@ -3,34 +3,39 @@ module gcalc (
     output [3:0] g
 );
 
-reg  [2:0] scale;
+reg doshift;
+reg sub;
+reg  [1:0] shiftby;
 reg  [2:0] addon;
-wire [3:0] mult_res;
 
+wire [3:0] shift_res = (doshift) ? i[3:0] << shiftby : 4'b0;
+wire [3:0] mult_res = (sub) ? shift_res - i[3:0] : shift_res + i[3:0];
 assign g = mult_res + addon;
-
-minimult mm (
-    .dataa (i[3:0]),
-    .datab (scale),
-    .result (mult_res)
-);
 
 always @(*) begin
     case (i[5:4])
         2'b00: begin
-            scale <= 3'd1;
+            doshift <= 1'b0;
+            sub <= 1'b0;
+            shiftby <= 2'b0;
             addon <= 3'd0;
         end
         2'b01: begin
-            scale <= 3'd5;
+            doshift <= 1'b1;
+            sub <= 1'b0;
+            shiftby <= 2'b10;
             addon <= 3'd1;
         end
         2'b10: begin
-            scale <= 3'd3;
+            doshift <= 1'b1;
+            sub <= 1'b0;
+            shiftby <= 2'b01;
             addon <= 3'd5;
         end
         2'b11: begin
-            scale <= 3'd7;
+            doshift <= 1'b1;
+            sub <= 1'b1;
+            shiftby <= 2'b11;
             addon <= 3'd0;
         end
     endcase

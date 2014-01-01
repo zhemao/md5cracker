073ea13c6d34a9142504b694ac981def6d11162d
Howard Mao <zhehao.mao@gmail.com>
2014-01-01 14:36:06 -0500

    change some radices in tcl script

--- sim/md5unit_sim.tcl
+++ sim/md5unit_sim.tcl
@@ -9,18 +9,20 @@ add wave {/md5unit_tb/md5/cc_sdata[0]}
 add wave -radix hexadecimal {/md5unit_tb/md5/cc_kdata[0]}
 add wave {/md5unit_tb/md5/cc_iaddr[0]}
 add wave {/md5unit_tb/md5/cc_gaddr[0]}
+
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/areg}
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/breg}
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/creg}
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/dreg}
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/adds}
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/rotated}
-add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/step}
-add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/stage}
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/adda}
 add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/addb}
-add wave {/md5unit_tb/md5/mccgen[0]/cc/t0}
-add wave {/md5unit_tb/md5/mccgen[0]/cc/t1}
-add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/ireg}
+add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/t0}
+add wave -radix hexadecimal {/md5unit_tb/md5/mccgen[0]/cc/t1}
+
+add wave {/md5unit_tb/md5/mccgen[0]/cc/step}
+add wave {/md5unit_tb/md5/mccgen[0]/cc/stage}
+add wave -radix unsigned {/md5unit_tb/md5/mccgen[0]/cc/ireg}
 
 run 5600 ns
